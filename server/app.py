dfd42424bec73b4dc9a79a6cf32ff8fa825c4678
Justin Steffen <jcsteffen03@gmail.com>
2024-08-06 14:28:32 -0600

    test passed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.all()
+        list =[]
+        for message in messages:
+            list.append(message.to_dict())
+        return list
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return new_message.to_dict(), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    if request.method == 'PATCH':
+        data = request.get_json()
+        if 'body' in data:
+            message.body = data['body']
+        if 'username' in data:
+            message.username = data['username']
+        db.session.commit()
+        return message.to_dict()
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
+
 
 if __name__ == '__main__':
     app.run(port=5555)
